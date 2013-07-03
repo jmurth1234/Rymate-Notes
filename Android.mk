445c14d9906f96f05d7d033836b98823928b607b
rymate1234 <supermarioryan@gmail.com>
2013-07-03 20:41:35 +0100

    k is fixed nao

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@ LOCAL_MODULE_TAGS := optional
 # Only compile source java files in this apk.
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := Notepadv3Solution
+LOCAL_PACKAGE_NAME := RymateNotes
 
 # Make the app build against the current SDK
 LOCAL_SDK_VERSION := current
