5d3d761f9d3fa5c67fd15e4dce9b6ce968cf5db9
rymate1234 <supermarioryan@gmail.com>
2015-05-29 14:48:42 +0100

    fix project

--- gradlew.bat
+++ gradlew.bat
@@ -1,8 +1,8 @@
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
-@rem
-@rem  Gradle startup script for Windows
-@rem
+@rem ##
+@rem ## Gradle startup script for Windows
+@rem ##
 @rem ##########################################################################
 
 @rem Set local scope for the variables with windows NT shell
